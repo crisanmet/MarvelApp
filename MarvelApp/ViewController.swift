38a5372cc860dc3d5f28e3d8eddd23ee51fd91ed
Cristian Sancricca <cristiansancricca@gmail.com>
2022-06-09 13:59:14 -0300

    Initial Commit

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Cristian Sancricca on 09/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
