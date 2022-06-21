ab778eea9b4608c3abb0214ca95905eecd35b95f
Cristian Sancricca <cristiansancricca@gmail.com>
2022-06-21 15:20:31 -0300

    remove comments

--- MarvelApp/App/Utils/ImageLoader.swift
+++ MarvelApp/App/Utils/ImageLoader.swift
@@ -13,30 +13,28 @@ class ImageLoader {
     
     func loadImage(_ url: URL, _ completion: @escaping (Result<UIImage, Error>) -> Void) -> UUID? {
 
-      // 1
+      
       if let image = loadedImages[url] {
         completion(.success(image))
         return nil
       }
 
-      // 2
       let uuid = UUID()
 
       let task = URLSession.shared.dataTask(with: url) { data, response, error in
-        // 3
+        
         defer {self.runningRequests.removeValue(forKey: uuid) }
 
-        // 4
+        
         if let data = data, let image = UIImage(data: data) {
           self.loadedImages[url] = image
           completion(.success(image))
           return
         }
 
-        // 5
+    
         guard let error = error else {
-          // without an image or an error, we'll just ignore this for now
-          // you could add your own special error cases for this scenario
+          
           return
         }
 
@@ -45,11 +43,11 @@ class ImageLoader {
           return
         }
 
-        // the request was cancelled, no need to call the callback
+        
       }
       task.resume()
 
-      // 6
+    
       runningRequests[uuid] = task
       return uuid
     }
